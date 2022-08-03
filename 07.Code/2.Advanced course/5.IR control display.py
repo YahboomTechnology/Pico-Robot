f9e670872e8f7637c6ff8fac785e51daf1881e6f
Yahboom Technology <35594203+YahboomTechnology@users.noreply.github.com>
2022-08-03 16:18:47 +0800

    Add files via upload

--- 07.Code/2.Advanced course/5.IR control display.py
+++ 07.Code/2.Advanced course/5.IR control display.py
@@ -14,122 +14,128 @@ while True:
     if value != None:
         print(value)
         #display press
-        if value == 69:
-            while value == 69:
+        if value == 0:
+            while value == 0:
                 value = Ir.Getir()
             oled.text('Press:Power', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 71:
-            while value == 71:
+        elif value == 1:
+            while value == 1:
                 value = Ir.Getir()
-            oled.text('Press:MENU', 0, 0)
+            oled.text('Press:Up', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 68:
-            while value == 68:
+        elif value == 2:
+            while value == 2:
                 value = Ir.Getir()
-            oled.text('Press:TEST', 0, 0)
+            oled.text('Press:Light', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 64:
-            while value == 64:
+        elif value == 4:
+            while value == 4:
                 value = Ir.Getir()
-            oled.text('Press:+', 0, 0)
+            oled.text('Press:Left', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 67:
-            while value == 67:
+        elif value == 5:
+            while value == 5:
                 value = Ir.Getir()
-            oled.text('Press:BACK', 0, 0)
+            oled.text('Press:Sound', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 7:
-            while value == 7:
+        elif value == 6:
+            while value == 6:
                 value = Ir.Getir()
-            oled.text('Press:<<', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 9:
-            while value == 9:
-                value = Ir.Getir()
-            oled.text('Press:>>', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 21:
-            while value == 21:
-                value = Ir.Getir()
-            oled.text('Press:>', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 25:
-            while value == 25:
-                value = Ir.Getir()
-            oled.text('Press:-', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 13:
-            while value == 13:
-                value = Ir.Getir()
-            oled.text('Press:C', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 22:
-            while value == 22:
-                value = Ir.Getir()
-            oled.text('Press:0', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 12:
-            while value == 12:
-                value = Ir.Getir()
-            oled.text('Press:1', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 24:
-            while value == 24:
-                value = Ir.Getir()
-            oled.text('Press:2', 0, 0)
-            oled.show()
-            oled.fill(0)
-        elif value == 94:
-            while value == 94:
-                value = Ir.Getir()
-            oled.text('Press:3', 0, 0)
+            oled.text('Press:Right', 0, 0)
             oled.show()
             oled.fill(0)
         elif value == 8:
             while value == 8:
                 value = Ir.Getir()
+            oled.text('Press:Turn Left', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 9:
+            while value == 9:
+                value = Ir.Getir()
+            oled.text('Press:Down', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 10:
+            while value == 10:
+                value = Ir.Getir()
+            oled.text('Press:Turn Right', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 12:
+            while value == 12:
+                value = Ir.Getir()
+            oled.text('Press:+', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 13:
+            while value == 13:
+                value = Ir.Getir()
+            oled.text('Press:0', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 14:
+            while value == 14:
+                value = Ir.Getir()
+            oled.text('Press:-', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 16:
+            while value == 16:
+                value = Ir.Getir()
+            oled.text('Press:1', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 17:
+            while value == 17:
+                value = Ir.Getir()
+            oled.text('Press:2', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 18:
+            while value == 18:
+                value = Ir.Getir()
+            oled.text('Press:3', 0, 0)
+            oled.show()
+            oled.fill(0)
+        elif value == 20:
+            while value == 20:
+                value = Ir.Getir()
             oled.text('Press:4', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 28:
-            while value == 28:
+        elif value == 21:
+            while value == 21:
                 value = Ir.Getir()
             oled.text('Press:5', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 90:
-            while value == 90:
+        elif value == 22:
+            while value == 22:
                 value = Ir.Getir()
             oled.text('Press:6', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 66:
-            while value == 66:
+        elif value == 24:
+            while value == 24:
                 value = Ir.Getir()
             oled.text('Press:7', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 82:
-            while value == 82:
+        elif value == 25:
+            while value == 25:
                 value = Ir.Getir()
             oled.text('Press:8', 0, 0)
             oled.show()
             oled.fill(0)
-        elif value == 74:
-            while value == 74:
+        elif value == 26:
+            while value == 26:
                 value = Ir.Getir()
             oled.text('Press:9', 0, 0)
             oled.show()
